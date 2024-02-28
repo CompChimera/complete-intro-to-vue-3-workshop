795091e20d27ca333520a4c31793cf0e09e53d63
Brittany Kelly <computationalchimera@gmail.com>
2024-02-28 10:47:07 -0400

    Component and props exercises

--- Shangrila-forum/src/App.vue
+++ Shangrila-forum/src/App.vue
@@ -1,51 +1,48 @@
 <script >
+import CharacterList from './components/CharacterList.vue'
+
 
 export default {
-  
-  data: () => ({
-            charactersList: [
-          { name: 'Rakuro', bio: 'In game name - Sunraku', powers: ['speed', 'strength']},
-          { name: 'Towa', bio: 'In game name: Authur Pencilgon', powers: ['strength', 'friendship'] },
-          { name: 'Rei', bio: 'In game name: Psyger-0', powers: ['strength   ', 'speed', 'cunning'] },
-          { name: 'Kei', bio: 'In game name: Oikatzo', powers: ['cunning', 'speed', 'famous'] }
-        ],
-            newCharacter : {
-              name: ''
-            },
-          favouriteList: []
-        }),
-        computed: {
-          powerStatistics() {
-            const powers = ['strength', 'cunning', 'friendship', 'famous']
-
-            const statistics = {
-              strength: 0,
-              cunning: 0,
-              friendship: 0,
-              famous: 0
-            }
-
-            this.charactersList.forEach(character => {
-              powers.forEach(power => {
-                if(character.powers.indexOf(power) > -1) {
-                  statistics[power] += 1
-                }
-              })
-            })
-
-            return statistics
-
-          }
+    data: () => ({
+        // charactersList: [
+        //     { name: 'Rakuro', bio: 'In game name - Sunraku', powers: ['speed', 'strength'] },
+        //     { name: 'Towa', bio: 'In game name: Authur Pencilgon', powers: ['strength', 'friendship'] },
+        //     { name: 'Rei', bio: 'In game name: Psyger-0', powers: ['strength   ', 'speed', 'cunning'] },
+        //     { name: 'Kei', bio: 'In game name: Oikatzo', powers: ['cunning', 'speed', 'famous'] }
+        // ],
+        charactersList: [],
+        newCharacter: {
+            name: ''
         },
-        methods: {
-          favouriteFlag(favouriteAdd, event) {
-              this.favouriteList.push(favouriteAdd) 
-          },
-          addNewCharacter() {
-            this.charactersList.push(this.newCharacter)
-            this.newCharacter = { name: '' }
-          }
+        favouriteList: []
+    }),
+    computed: {
+        powerStatistics() {
+            const powers = ['strength', 'cunning', 'friendship', 'famous'];
+            const statistics = {
+                strength: 0,
+                cunning: 0,
+                friendship: 0,
+                famous: 0
+            };
+            this.charactersList.forEach(character => {
+                powers.forEach(power => {
+                    if (character.powers && character.powers.indexOf(power) > -1) {
+                        statistics[power] += 1;
+                    }
+                });
+            });
+            return statistics;
         }
+    },
+    methods: {
+
+        addNewCharacter() {
+            this.charactersList.push(this.newCharacter);
+            this.newCharacter = { name: '' };
+        }
+    },
+    components: { CharacterList }
 }
 </script>
 
@@ -54,19 +51,9 @@ export default {
 
 <ul>
   <li v-for="(stat, type) in powerStatistics" :key="`character-${stat}-${type}`"> <span>{{ type[0].toUpperCase() + type.substring(1) }} : {{ stat }} </span></li>
-
 </ul>
+<CharacterList :charactersList="charactersList" :favouriteList="favouriteList"/>
 
-<p v-if="charactersList.length > 0">Main Characters: </p>
-<p v-else>To be announced!</p>
-<ul>
-  <li v-if="charactersList.length > 0" v-for="(character, index) in charactersList" :key="`character-${index}`">
-    <span v-for="(value, key) in character" :key="`characterslist-detail-${key}-${value}`" style="padding-left: 10px;">
-      <span style="font-weight: bold">{{ key[0].toUpperCase() + key.substring(1) }}</span> : {{ value }} 
-    </span>
-    <button @click="favouriteFlag(character, $event)" style="margin-left: 20px; margin-top: 20px;"> ⭐ Favorite </button>
-  </li>
-</ul>
 <h2>New Character (Limited to names)</h2>
 <div class="new-char__wrapper" style="padding: 20px; border: 1px solid black'">
   <label for="character-name">Name: </label>
