5e502d243cc2f340154b5520eeeaec28e89f515f
Brittany Kelly <computationalchimera@gmail.com>
2024-02-29 22:32:50 -0400

    slots - switch template order

--- Shangrila-forum/src/components/BaseLayout.vue
+++ Shangrila-forum/src/components/BaseLayout.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="wrapper">
-    <main class="main">
-      <slot name="main" />
-    </main>
     <aside class="sidebar">
       <slot name="sidebar" />
     </aside>
+    <main class="main">
+      <slot name="main" />
+    </main>
+
   </div>
   <footer class="footer">
     <slot name="footer" />
@@ -20,16 +21,19 @@
 }
 .sidebar {
   border: 2px solid red;
+  grid-area: 'sidebar';
   padding: 20px;
 }
 
 .main {
   border: 2px solid green;
+  grid-area: 'main';
   padding: 20px;
 }
 
 .footer {
   border: 2px solid blue;
+  grid-area: footer;
   padding: 20px;
 }
 </style>
