a55a979d0b5a4e0e8fe1bad10e47046ebb71a059
Brittany Kelly <computationalchimera@gmail.com>
2024-03-01 20:24:31 -0400

    update pokedex to use setup hook to fetch API data and use Suspense component

--- PokeApi/src/App.vue
+++ PokeApi/src/App.vue
@@ -1,25 +1,23 @@
 <script>
 //https://pokeapi.co/api/v2/pokemon?limit=151
+import Pokedex from './components/Pokedex.vue'
 
 export default {
-  data: () => ({
-    pokedex: [1,2,3]
-  }),
-  methods: {
-    async fetchPokemon() {
-      this.pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then(response => response.json())
-    }
-  },
-  created() {
-    this.fetchPokemon();
+  components: {
+    Pokedex,
   }
 }
 </script>
 
 <template>
   <h1> New App</h1>
-  <pre>{{  pokedex  }}</pre>
-  <button @click="fetchPokemon">Fetch Pokemon</button>
+  <Suspense>
+    <Pokedex />
+
+    <template v-slot:fallback >
+      Loading Pokedex Data...
+    </template>
+  </Suspense>
 </template>
 
 <style></style>
